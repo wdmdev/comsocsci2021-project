4e9dc200e65a06b389b8ba9553a6706067f48390
Lukas Leindals <s183920@student.dtu.dk>
2021-04-07 12:01:15 +0200

    clean books

---
+++ clean_books.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+import re
+import pickle
+
+def clean_book(filename):
+    with open(filename, encoding='utf-8') as bad_html:
+        soup = BeautifulSoup(bad_html, features="html.parser")
+    
+    pretty = soup.prettify()
+    start = re.search(r'<pre>.*', pretty).span()[1]
+    end = re.search(r'<.pre>.*', pretty).span()[0]
+    text = pretty[start:end]
+    text = text.lstrip()
+    text = text.rstrip()
+    return text
+
+
+
+# fname = "Data/Book 1 - The Philosopher's Stone_djvu.txt.html"
+# text = clean_book(fname) 
+# # with open ("test.txt", "w", encoding='utf-8') as test:
+# #     test.write(text)
+
+# re_page = r"Page \| \d+ .+"
+# print(re.search(re_page, text))
+
+with open("Data/chapter_dataframe.pkl", "rb") as file:
+    chapters = pickle.load(file)
+
+print(chapters)
